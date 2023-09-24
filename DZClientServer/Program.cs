45fa17121fda7993d0cf66b6c27612bab3e6791c
Anton <laant@mail.ru>
2023-09-24 22:25:05 +0500

    Добавьте файлы проекта.

---
+++ DZClientServer/Program.cs
@@ -0,0 +1,37 @@
+﻿// клиент 
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const string ip = "127.0.0.1";
+const int port = 8080;
+IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ip), port);
+Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+string message = " ";
+while(message!="")
+{
+    Console.WriteLine("Введите текст сообщения: ");
+    message = Console.ReadLine();
+    var data = Encoding.UTF8.GetBytes(message);
+    clientSocket.Connect(ep);
+    clientSocket.Send(data); // отправка данных
+
+    var buffer = new byte[256];
+    // int size = 0;
+    var answer = new StringBuilder();
+
+    do
+    {
+        clientSocket.Receive(buffer);// для клиента
+        answer.Append(Encoding.UTF8.GetString(buffer,0, 1));
+        
+    } while (clientSocket.Available > 0);
+
+    Console.WriteLine(answer.ToString());
+
+
+}
+clientSocket.Shutdown(SocketShutdown.Both);
+clientSocket.Close();
+Console.ReadLine();
---
+++ DZServer/Program.cs
@@ -0,0 +1,67 @@
+﻿// Сервер
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+const string ip = "127.0.0.1";
+const int port = 8080;
+
+IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+while (true)
+{
+    try
+    {
+        var serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        serverSocket.Bind(serverEndPoint);
+        serverSocket.Listen(5);
+
+        Console.WriteLine("Сервер запущен. Ожидание подключений... ");
+
+        while (true)
+        {
+
+            var listener = serverSocket.Accept();// для сервера
+            var buffer = new byte[256];
+            int size;
+            var data = new StringBuilder();
+
+            do
+            {
+
+                size = listener.Receive(buffer);
+                data?.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                if (data == null)
+                    Console.WriteLine("gecnf  строка пришла"); 
+
+            } while (serverSocket.Available > 0);
+
+            Console.WriteLine("Сервер получает: " + data);
+            listener.Send(Encoding.UTF8.GetBytes($"Сервер получил ({data}) и отвечает: \nПривет клиент"));
+
+                listener.Shutdown(SocketShutdown.Both);
+            listener.Close();
+            Console.WriteLine("Сервер закончил работу.");
+
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
